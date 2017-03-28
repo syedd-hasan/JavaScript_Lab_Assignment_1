13d707acf358beb4c2af419836e70914e5528b5f
Syed Hasan <syedhasan200@gmail.com>
2017-03-29 00:30:30 +0500

    Final Project

--- app.js
+++ app.js
@@ -85,10 +85,10 @@ document.write("<a href = 'index.html'><button>Go Back</button></a>");
 function third(){
 var city = prompt("Please Enter Your City").toLowerCase();
 if(city == "karachi"){
-    document.write("<h3>Welcome To The City Of Lights</h3>");
+    document.write("<p>Welcome To The City Of Lights</p>");
 }
 else{
-    document.write("<h3>Welcome To "+city.charAt(0).toUpperCase() + city.slice(1)+"</h3>"); /*Capatilizing First Word of City Name*/
+    document.write("<p>Welcome To "+city.charAt(0).toUpperCase() + city.slice(1)+"</p>"); /*Capatilizing First Word of City Name*/
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -97,13 +97,13 @@ document.write("<a href = 'index.html'><button>Go Back</button></a>");
 function fourth(){
 var gender = prompt("Enter Your Gender, You Can write 'M' for Male and 'F' for Female").toLowerCase();
 if(gender == "male" || gender == "m"){
-    document.write("<h3>Good Morning Sir</h3>");
+    document.write("<p>Good Morning Sir</p>");
 }
 else if(gender == "female" || gender == "f"){
-    document.write("<h3>Good Morning Ma'am</h3>");
+    document.write("<p>Good Morning Ma'am</p>");
 }
 else{
-    document.write("<h3>Sorry your Gender is not Specified</h3>");
+    document.write("<p>Sorry your Gender is not Specified</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -111,10 +111,10 @@ document.write("<a href = 'index.html'><button>Go Back</button></a>");
 /*Fifth Program */
 function fifth(){
 var color = prompt("Which Color's Light of Traffic Signal is Illuminating?").toLowerCase();
-if(color == "red"){document.write("<h3>Vehicles must stop</h3>");}
-else if(color == "yellow"){document.write("<h3>Vehicles should get ready to move</h3>");}
-else if(color == "green"){document.write("<h3>Vehicles can move now</h3>");}
-else{document.write("<h3>Hold On! "+color+" is not a Traffic Signal's color</h3>");}
+if(color == "red"){document.write("<p>Vehicles must stop</p>");}
+else if(color == "yellow"){document.write("<p>Vehicles should get ready to move</p>");}
+else if(color == "green"){document.write("<p>Vehicles can move now</p>");}
+else{document.write("<p>Hold On! "+color+" is not a Traffic Signal's color</p>");}
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
 
@@ -126,13 +126,13 @@ var current_age = +prompt("Enter Current Age");
 console.log(max_age);
 console.log(current_age);
 if(current_age <= max_age && max_age!=0 && current_age!=0){
-    document.write("<h3>You Are Welcome</h3>");
+    document.write("<p>You Are Welcome</p>");
 }
 else if(isNaN(max_age)||isNaN(current_age)||max_age==0||current_age==0){
-    document.write("<h3>Kindly Enter any Valid Number on Both Maximum and Current Age</h3>");
+    document.write("<p>Kindly Enter any Valid Number on Both Maximum and Current Age</p>");
 }
 else{
-    document.write("<h3>Sorry You Can't Enter</h3>");
+    document.write("<p>Sorry You Can't Enter</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -141,13 +141,13 @@ document.write("<a href = 'index.html'><button>Go Back</button></a>");
 function seventh(){
 var fuel = +prompt("Enter Amount of Remaining Fuel in Litres");
 if(isNaN(fuel)||fuel == 0){
-    document.write("<h3>Please Enter Valid Number</h3>");
+    document.write("<p>Please Enter Valid Number</p>");
 }
 else if(fuel <= 0.25){
-    document.write("<h3>Please Refill The Fuel In Your Car</h3>");
+    document.write("<p>Please Refill The Fuel In Your Car</p>");
 }
 else{
-    document.write("<h3>Enough Fuel</h3>");
+    document.write("<p>Enough Fuel</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -234,24 +234,24 @@ else if(percentage <= 60 ){
 }
 
 if(total < (first_sub+second_sub+third_sub)){   /*If total marks is less than sum of all three subjects */
-    document.write("<h3>Total Marks Should Be Greater Than Sum of All Three Marks</h3>");
+    document.write("<p>Total Marks Should Be Greater Than Sum of All Three Marks</p>");
 }
 else if(isNaN(first_sub) || isNaN(second_sub) || isNaN(third_sub)){ /*If any of three subject's entered marks is not a number (see JS function (isNaN))*/
-    document.write("<h3>Kindly Enter Valid Numbers on all Three Subjects</h3>");
+    document.write("<p>Kindly Enter Valid Numbers on all Three Subjects</p>");
 }
 else if(total == 0){    /*If total marks Entered is Zero, as it can't be */
-        document.write("<h3>Kindly Enter Total Marks Greater Than Zero</h3>");
+        document.write("<p>Kindly Enter Total Marks Greater Than Zero</p>");
 }
 else if(trigger == 1){  /*Displaying error if percentage is greator than 100 */
-    document.write("<h3>100% cannot be obtained</h3>");
+    document.write("<p>100% cannot be obtained</p>");
 }
 else{       /*Printing Marksheet After All conditions are met */
     document.write("<h2>Marksheet</h2>");
-    document.write("<h3>Total Marks: "+total+"</h3>");
-    document.write("<h3>Marks Obtained: "+(first_sub+second_sub+third_sub)+"</h3>");
-    document.write("<h3>Percentage: "+percentage+"%</h3>");
-    document.write("<h3>Grade: "+grade+"</h3>");
-    document.write("<h3>Remarks: "+remarks+"</h3>");
+    document.write("<p>Total Marks: "+total+"</p>");
+    document.write("<p>Marks Obtained: "+(first_sub+second_sub+third_sub)+"</p>");
+    document.write("<p>Percentage: "+percentage+"%</p>");
+    document.write("<p>Grade: "+grade+"</p>");
+    document.write("<p>Remarks: "+remarks+"</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -271,13 +271,13 @@ if(total_cost > 2000){
     discount = total_cost*(1-(10/100));
 }
 document.write("<h2>Shopping Cart</h2>");
-document.write("<h3>Price of "+item_1+" is "+price_1+"</h3>");
-document.write("<h3>Quantity of "+item_1+" is "+quantity_1+"</h3>");
-document.write("<h3>Price of "+item_2+" is "+price_2+"</h3>");
-document.write("<h3>Quantity of "+item_2+" is "+quantity_2+"</h3><br/>");
-document.write("<h3>Shipping Charges "+shipping+"</h3><br/>");
-document.write("<h3>Total cost of your Order is "+total_cost+" PKR</h3>");
-document.write("<h3>Discounted Price is "+discount+" PKR</h3>");
+document.write("<p>Price of "+item_1+" is "+price_1+"</p>");
+document.write("<p>Quantity of "+item_1+" is "+quantity_1+"</p>");
+document.write("<p>Price of "+item_2+" is "+price_2+"</p>");
+document.write("<p>Quantity of "+item_2+" is "+quantity_2+"</p><br/>");
+document.write("<p>Shipping Charges "+shipping+"</p><br/>");
+document.write("<p>Total cost of your Order is "+total_cost+" PKR</p>");
+document.write("<p>Discounted Price is "+discount+" PKR</p>");
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
 
@@ -286,13 +286,13 @@ function eleventh(){
 var s_number = 3;
 var guess_number = +prompt("Guess a number from 1 to 10");
 if(guess_number === s_number){
-     document.write("<h3>Bingo! Correct Answer</h3>");
+     document.write("<p>Bingo! Correct Answer</p>");
 }
 else if((guess_number+1) == s_number || (guess_number-1) == s_number){
-    document.write("<h3>You were close Enough!</h3>");
+    document.write("<p>You were close Enough!</p>");
 }
 else{
-     document.write("<h3>Sorry! Wrong Answer</h3>");
+     document.write("<p>Sorry! Wrong Answer</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -312,16 +312,16 @@ var score_1 = +prompt("Enter Score Of First Team");
 var team_2 = prompt("Enter Name of Second Team");
 var score_2 = +prompt("Enter Score of Second Team");
 if(score_1>score_2){
-    document.write("<h3>"+team_1+" has won the game</h3>");
+    document.write("<p>"+team_1+" has won the game</p>");
 }
 else if(score_1 < score_2){
-    document.write("<h3>"+team_2+" has won the game</h3>");
+    document.write("<p>"+team_2+" has won the game</p>");
 }
 else if(score_1===score_2){
-    document.write("<h3>The Game is a Tie</h3>");
+    document.write("<p>The Game is a Tie</p>");
 }
 else{
-    document.write("<h3>Wrong Input</h3>");
+    document.write("<p>Wrong Input</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -345,20 +345,20 @@ if(bool == true || bool == false){
 /*fifteenth Program */
 function fifteenth(){
 var input_4 = +prompt("Enter a Number");
-if(input_4%2 == 0){document.write("<h3>Your Number is Even Number</h3>");}
-else if(input_4%3 == 0){document.write("<h3>Your Number is Odd Number</h3>");}
-else{document.write("<h3>Wrong Input</h3>");}
+if(input_4%2 == 0){document.write("<p>Your Number is Even Number</p>");}
+else if(input_4%3 == 0){document.write("<p>Your Number is Odd Number</p>");}
+else{document.write("<p>Wrong Input</p>");}
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
 /*sixteenth Program */
 function sixteenth(){
 var temperature = +prompt("Enter the degree of Atmospheric Temperature");
-if(temperature>=40){document.write("<h3>It's Too Hot Outside, Better to Stay at Home</h3>");}
-else if(temperature>=30){document.write("<h3>The Weather today is Normal.</h3>");}
-else if(temperature>=20){document.write("<h3>Today’s Weather is cool.</h3>");}
-else if(temperature>=10){document.write("<h3>OMG! Today’s weather is so Cool.</h3>");}
+if(temperature>=40){document.write("<p>It's Too Hot Outside, Better to Stay at Home</p>");}
+else if(temperature>=30){document.write("<p>The Weather today is Normal.</p>");}
+else if(temperature>=20){document.write("<p>Today’s Weather is cool.</p>");}
+else if(temperature>=10){document.write("<p>OMG! Today’s weather is so Cool.</p>");}
 else{
-    document.write("<h3>Wrong Input.</h3>");
+    document.write("<p>Wrong Input.</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -368,12 +368,12 @@ function seventeenth(){
 var first_num = +prompt("Enter First Number");
 var second_num = +prompt("Enter Second Number");
 var operator = prompt("Enter symbol of operation e.g: (+, -, *, /, %)");
-if(operator == '+'){document.write("<h3>"+first_num+operator+second_num+" is "+(first_num+second_num)+"</h3>");}
-else if(operator == '-'){document.write("<h3>"+first_num+operator+second_num+" is "+(first_num-second_num)+"</h3>");}
-else if(operator == '*'){document.write("<h3>"+first_num+operator+second_num+" is "+(first_num*second_num)+"</h3>");}
-else if(operator == '/'){document.write("<h3>"+first_num+operator+second_num+" is "+(first_num/second_num)+"</h3>");}
-else if(operator == '%'){document.write("<h3>"+first_num+operator+second_num+" is "+(first_num%second_num)+"</h3>");}
-else{document.write("<h3>Wrong Input</h3>");}
+if(operator == '+'){document.write("<p>"+first_num+operator+second_num+" is "+(first_num+second_num)+"</p>");}
+else if(operator == '-'){document.write("<p>"+first_num+operator+second_num+" is "+(first_num-second_num)+"</p>");}
+else if(operator == '*'){document.write("<p>"+first_num+operator+second_num+" is "+(first_num*second_num)+"</p>");}
+else if(operator == '/'){document.write("<p>"+first_num+operator+second_num+" is "+(first_num/second_num)+"</p>");}
+else if(operator == '%'){document.write("<p>"+first_num+operator+second_num+" is "+(first_num%second_num)+"</p>");}
+else{document.write("<p>Wrong Input</p>");}
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
 
@@ -381,16 +381,16 @@ document.write("<a href = 'index.html'><button>Go Back</button></a>");
 function eighteenth(){
 var day = prompt("Enter Day Name").toLowerCase();
 if(day == "monday" ||day == "tuesday" ||day == "wednesday" ||day == "thursday" ||day == "friday"){
-    document.write("<h3>It's a Week Day</h3>");
+    document.write("<p>It's a Week Day</p>");
 }
 else if(day == "saturday"){
-    document.write("<h3>It's Weekend</h3>");
+    document.write("<p>It's Weekend</p>");
 }
 else if(day == "sunday"){
-    document.write("<h3>Yay! It's a holiday</h3>");
+    document.write("<p>Yay! It's a holiday</p>");
 }
 else{
-    document.write("<h3>Wrong Input</h3>");
+    document.write("<p>Wrong Input</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -398,8 +398,8 @@ document.write("<a href = 'index.html'><button>Go Back</button></a>");
 /*nineteenth Program */
 function nineteenth(){
 var score = +prompt("Enter Your Score");
-if(score > 50){document.write("<h3>You Are Passed</h3>");}
-else{document.write("<h3>Try Again</h3>");}
+if(score > 50){document.write("<p>You Are Passed</p>");}
+else{document.write("<p>Try Again</p>");}
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
 
@@ -407,11 +407,11 @@ document.write("<a href = 'index.html'><button>Go Back</button></a>");
 function twentieth(){
 var no_1 = +prompt("Enter First Number");
 var no_2 = +prompt("Enter Second Number");
-if(no_1>no_2){document.write("<h3>The greater Number of "+no_1+" and "+no_2+" is "+no_1+"</h3>")}
-else if(no_1 < no_2){document.write("<h3>The greater Number of "+no_1+" and "+no_2+" is "+no_2+"</h3>")}
-else if(no_1===no_2){document.write("<h3>The Numbers "+no_1+" and "+no_2+" are Equal</h3>")}
+if(no_1>no_2){document.write("<p>The greater Number of "+no_1+" and "+no_2+" is "+no_1+"</p>")}
+else if(no_1 < no_2){document.write("<p>The greater Number of "+no_1+" and "+no_2+" is "+no_2+"</p>")}
+else if(no_1===no_2){document.write("<p>The Numbers "+no_1+" and "+no_2+" are Equal</p>")}
 else{
-    document.write("<h3>Wrong Input</h3>");
+    document.write("<p>Wrong Input</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -419,20 +419,20 @@ document.write("<a href = 'index.html'><button>Go Back</button></a>");
 /*twenty-first Program */
 function twenty_first(){
 var language_code = prompt("Enter Language Code (ur for Urdu, en for English, de for German)").toLowerCase();
-if(language_code == "ur"){document.write("<h3>ہیلو د نیا</h3>");}
-else if(language_code == "en"){document.write("<h3>Hello World!</h3>");}
-else if(language_code == "de"){document.write("<h3>Hallo Welt!</h3>");}
-else{document.write("<h3>Hello World!</h3>");}
+if(language_code == "ur"){document.write("<p>ہیلو د نیا</p>");}
+else if(language_code == "en"){document.write("<p>Hello World!</p>");}
+else if(language_code == "de"){document.write("<p>Hallo Welt!</p>");}
+else{document.write("<p>Hello World!</p>");}
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
 
 /*twenty-second Program */
 function twenty_second(){
 var noumber = +prompt("Enter Any Integer");
-if(noumber >= 0){document.write("<h3>That is a Positive Number</h3>");}
-else if(noumber < 0){document.write("<h3>That is a Negative Number</h3>");}
+if(noumber >= 0){document.write("<p>That is a Positive Number</p>");}
+else if(noumber < 0){document.write("<p>That is a Negative Number</p>");}
 else{
-    document.write("<h3>Wrong Input</h3>");
+    document.write("<p>Wrong Input</p>");
 }
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
@@ -441,8 +441,8 @@ document.write("<a href = 'index.html'><button>Go Back</button></a>");
 function twenty_third(){
 var noun = prompt("Please Enter a Noun");
 var n_no = +prompt("Please Enter a Number");
-if(n_no > 1){document.write("<h3>"+n_no+" "+noun+"s</h3>");}
-else if(n_no === 1){document.write("<h3>"+n_no+" "+noun+"</h3>");}
-else{document.write("<h3>Wrong Input</h3>");}
+if(n_no > 1){document.write("<p>"+n_no+" "+noun+"s</p>");}
+else if(n_no === 1){document.write("<p>"+n_no+" "+noun+"</p>");}
+else{document.write("<p>Wrong Input</p>");}
 document.write("<a href = 'index.html'><button>Go Back</button></a>");
 }
